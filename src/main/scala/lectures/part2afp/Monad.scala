01d9a543a33c4bb6a628213553a6b1189ed6e4a9
Shaw Lu <shawlu95@gmail.com>
2024-06-02 02:05:34 -0700

    Monad exercise, Lazy[T]

--- src/main/scala/lectures/part2afp/Monad.scala
+++ src/main/scala/lectures/part2afp/Monad.scala
@@ -57,4 +57,45 @@ object Monad extends App {
   }
 
   println(attempt)
+
+  /*
+  Exercise:
+  1) implement a Lazy[T] monad = computation which
+
+  only excecutes when needed
+    unit/apply
+    flatMap
+
+  2) Monads = unit + flatMap
+     Monads = unit + map + flatten
+
+  Given a correctly implemented Monad with flatMap
+  Implement it using map and flatten instead.
+
+  Monad[T] {
+    def flatMap[B]{f: T => Monad[B]): Monad[B] = ... (implemented)
+
+    def map[B}(f: T => B): Monad[B] = ???
+    def flatten(m: Monad[Monad[T]]): Monad[T] = ???
+  */
+
+  // pass value by name, which prevents evaluation when constructed
+  class Lazy[+A](value: => A) {
+    def use: A = value
+    def flatMap[B](f: A => Lazy[B]): Lazy[B] = f(value)
+  }
+
+  object Lazy {
+    def apply[A](value: => A): Lazy[A] = new Lazy(value)
+  }
+
+  // test Lazy monad
+  val lazyInstance = Lazy {
+    // should not see lyric printed when constructed
+    println("Today I don't feel like doing anything")
+    42
+  }
+
+  // should see both the lyric and 42
+  print(lazyInstance.use)
 }
