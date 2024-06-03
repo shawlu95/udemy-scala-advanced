3fd47b6ea0f7849e6b081ded1cdc74a3d6ce1680
Shaw Lu <shawlu95@gmail.com>
2024-06-03 03:25:07 -0700

    futures, functional programming way (callback hell)

--- src/main/scala/lectures/part3concurrency/FuturesPromises.scala
+++ src/main/scala/lectures/part3concurrency/FuturesPromises.scala
@@ -1,8 +1,7 @@
 package lectures.part3concurrency
 
 import scala.concurrent.Future
-import scala.util.Success
-import scala.util.Failure
+import scala.util.{Failure, Random, Success}
 
 // handle thread allocation for futures, passed as implicit param by compiler
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -30,4 +29,49 @@ object FuturesPromises extends App {
   })
 
   Thread.sleep(3000)
+
+  // mini social network
+  case class Profile(id: String, name: String) {
+    def poke(anotherProfile: Profile) = println(s"${this.name} poking ${anotherProfile.name} ")
+  }
+
+  object SocialNetwork {
+    // a map database
+    val names = Map(
+      "fb.id.1-zuck" -> "Mark",
+      "fb.id.2-bill" -> "Bill",
+      "fb.id.0-dummy" -> "Dummy"
+    )
+    val friends = Map(
+      "fb.id.1-zuck" -> "fb.id.2-bill"
+    )
+    val random = new Random()
+
+    // simulate API fetch from database
+    def fetchProfile(id: String): Future[Profile] = Future {
+      Thread.sleep(random.nextInt(300))
+      Profile(id, names(id))
+    }
+
+    def fetchBestFriends(profile: Profile): Future[Profile] = Future {
+      Thread.sleep(random.nextInt(300))
+      val id = friends(profile.id)
+      Profile(id, names(id))
+    }
+  }
+
+  // client-side code
+  val mark = SocialNetwork.fetchProfile("fb.id.1-zuck")
+  mark.onComplete {
+    case Success(markProfile) => {
+      val bill = SocialNetwork.fetchBestFriends(markProfile)
+      bill.onComplete {
+        case Success(billProfile) => markProfile.poke(billProfile)
+        case Failure(e) => e.printStackTrace()
+      }
+    }
+    case Failure(e) => e.printStackTrace()
+  }
+  Thread.sleep(1000)
 }
+
